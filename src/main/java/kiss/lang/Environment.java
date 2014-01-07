d683d13960099d2373432fc1beef08cf616383e3
mikera <mike.r.anderson.13@gmail.com>
2014-01-07 14:53:01 +0000

    handle basic environment mappings

--- src/main/java/kiss/lang/Environment.java
+++ src/main/java/kiss/lang/Environment.java
@@ -7,28 +7,34 @@ import clojure.lang.IMapEntry;
 import clojure.lang.IPersistentCollection;
 import clojure.lang.IPersistentMap;
 import clojure.lang.ISeq;
+import clojure.lang.PersistentHashMap;
+import clojure.lang.RT;
 import clojure.lang.Symbol;
 
 public class Environment extends APersistentMap {
 	
 	public static final Environment EMPTY = new Environment();
+	
+	public final PersistentHashMap map;
 
+	public Environment() {
+		map=PersistentHashMap.EMPTY;
+	}
+	
 	@Override
 	public IPersistentMap assoc(Object key, Object val) {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public IPersistentMap assocEx(Object key, Object val) {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public IPersistentMap without(Object key) {
 		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
@@ -39,44 +45,43 @@ public class Environment extends APersistentMap {
 
 	@Override
 	public boolean containsKey(Object key) {
-		// TODO Auto-generated method stub
-		return false;
+		return map.containsKey(key);
 	}
 
 	@Override
 	public IMapEntry entryAt(Object key) {
-		// TODO Auto-generated method stub
-		return null;
+		Mapping m=(Mapping) map.get(key);
+		if (m==null) return null;
+		return m.toMapEntry(key);
 	}
 
 	@Override
 	public int count() {
-		// TODO Auto-generated method stub
-		return 0;
+		return map.count();
 	}
 
 	@Override
 	public IPersistentCollection empty() {
-		// TODO Auto-generated method stub
-		return null;
+		return EMPTY;
 	}
 
 	@Override
 	public ISeq seq() {
-		// TODO Auto-generated method stub
-		return null;
+		return RT.seq(iterator());
 	}
 
 	@Override
 	public Object valAt(Object key) {
-		// TODO Auto-generated method stub
-		return null;
+		Mapping m=(Mapping) map.get(key);
+		if (m==null) return null;
+		return m.getValue();
 	}
 
 	@Override
 	public Object valAt(Object key, Object notFound) {
-		// TODO Auto-generated method stub
-		return null;
+		Mapping m=(Mapping) map.get(key);
+		if (m==null) return notFound;
+		return m.getValue();
 	}
 
 }
--- src/main/java/kiss/lang/Mapping.java
+++ src/main/java/kiss/lang/Mapping.java
@@ -1,7 +1,10 @@
 package kiss.lang;
 
+import clojure.lang.IMapEntry;
+
 /**
  * A kiss environement mapping
+ * 
  * 
  * @author Mike
  *
@@ -9,4 +12,48 @@ package kiss.lang;
 public class Mapping {
 	Object type;
 	Object value;
+	
+	public Object getValue() {
+		return value;
+	}
+
+	public IMapEntry toMapEntry(Object key) {
+		return new Entry(key,value);
+	}
+	
+	public static class Entry implements IMapEntry {
+		private Object key;
+		private Object value;
+
+		public Entry(Object key, Object value) {
+			this.key=key;
+			this.value=value;
+		}
+
+		@Override
+		public Object getKey() {
+			return key;
+		}
+
+		@Override
+		public Object getValue() {
+			return value;
+		}
+
+		@Override
+		public Object setValue(Object value) {
+			throw new UnsupportedOperationException("Mapping entry is immutable!");
+		}
+
+		@Override
+		public Object key() {
+			return key;
+		}
+
+		@Override
+		public Object val() {
+			return value;
+		}
+		
+	}
 }
