e4e052caa310dcb6ca0ec74a4ecc0476fe1c17bd
mikera <mike.r.anderson.13@gmail.com>
2014-01-07 15:51:07 +0000

    docs

--- src/main/java/kiss/lang/Type.java
+++ src/main/java/kiss/lang/Type.java
@@ -10,10 +10,24 @@ public abstract class Type {
 		return new JavaType(val.getClass());
 	}
 	
+	/**
+	 * Check if an object is an instance of this type
+	 * 
+	 * @param o
+	 * @return
+	 */
 	public abstract boolean checkInstance(Object o);
 	
+	/**
+	 * Returns the most specific Java class or interface that can represent all instances of this type
+	 * @return
+	 */
 	public abstract Class<?> getJavaType();
 	
+	/**
+	 * Returns true if this is a JVM primitive type
+	 * @return
+	 */
 	public boolean isPrimitive() {
 		return false;
 	}
