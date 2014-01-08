e54071605016b75ee522e02eb5800fc810d1fc9d
mikera <mike.r.anderson.13@gmail.com>
2014-01-08 15:31:23 +0000

    more lambda tests

--- src/test/clojure/kiss/test/test_lambda.clj
+++ src/test/clojure/kiss/test/test_lambda.clj
@@ -3,15 +3,24 @@
   (:use [mikera.cljutils error])
   (:use kiss.core))
 
+(deftest test-lambda-application
+  (testing "Apply a lambda to select a single argument"
+    (is (== 4 (kiss ((fn [x y] x) 4 5))))))
+
+(deftest test-higher-order
+  (testing "Higher order functions returning a lambda"
+    (is (== 4 (kiss (((fn [f] (fn [g] 4)) 5) 6))))))
+
 (deftest test-clojure-iterop
-  (testing "lambdas produce Clojure fns"
+  (testing "Kiss lambdas produce Clojure fns"
     (let [kfn (kiss (fn [] 3))]
       (is (== 3 (kfn))))
     (let [kfn2 (kiss (fn [x y] (clojure.core/+ x y)))]
       (is (== 3 (kfn2 1 2)))))
-  (testing "Clojure functions work in lambdas"
+  (testing "Clojure functions work in Kiss lambdas"
     (is (== 3 (kiss ((fn [] (clojure.core/+ 1 2))))))))
 
 (deftest test-arity-error
-  (is (error? (kiss ((fn [] 3) "foo"))))
-  (is (error? (kiss ((fn [x] 3))))))
+  (testing "Arity errors result in an exception"
+    (is (error? (kiss ((fn [] 3) "foo"))))
+    (is (error? (kiss ((fn [x] 3)))))))
