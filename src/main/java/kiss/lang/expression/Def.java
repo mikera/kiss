a45c33f6cddd82d69fe684b9fe51334331d76aa6
mikera <mike.r.anderson.13@gmail.com>
2014-01-14 04:08:35 +0800

    Add Def expression type

---
+++ src/main/java/kiss/lang/expression/Def.java
@@ -0,0 +1,42 @@
+package kiss.lang.expression;
+
+import clojure.lang.IPersistentMap;
+import clojure.lang.IPersistentSet;
+import clojure.lang.Symbol;
+import kiss.lang.Environment;
+import kiss.lang.Expression;
+import kiss.lang.Type;
+
+public class Def extends Expression {
+
+	private final Symbol sym;
+	private final Expression body;
+	
+	private Def(Symbol sym, Expression body) {
+		this.sym=sym;
+		this.body=body;
+	}
+	
+	@Override
+	public Type getType() {
+		return body.getType();
+	}
+
+	@Override
+	public Expression specialise(Type type) {
+		Expression b=body.specialise(type);
+		if ((b==body)||(b==null)) return this;
+		return new Def(sym,b);
+	}
+
+	@Override
+	public Environment compute(Environment d, IPersistentMap bindings) {
+		return d.define(sym,body,bindings);
+	}
+	
+	@Override
+	public IPersistentSet getFreeSymbols(IPersistentSet s) {
+		s=body.getFreeSymbols(s);
+		return s;
+	}
+}
