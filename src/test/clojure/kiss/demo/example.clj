6233b772c53b6c8c5df3acf587fa497470bcb278
mikera <mike.r.anderson.13@gmail.com>
2014-01-14 20:25:03 +0800

    example updates

--- src/test/clojure/kiss/demo/example.clj
+++ src/test/clojure/kiss/demo/example.clj
@@ -7,11 +7,15 @@
 
 ;; define an environment to work with
 (def env (environment {foo 1, 
-                       bar 2,
+                       bar 3,
                        f clojure.core/inc
                        baz (clojure.core/+ foo bar)}))
 
-;; === Environments are specialised Clojure maps
+;; ================================================= 
+;; Environments are specialised Clojure maps
+
+(map? env)
+; => true
 
 (env 'foo)
 ; => 1
@@ -22,12 +26,37 @@
 (class env)
 ; => kiss.lang.Environment
 
-;; === Expression results are computed as values
+;; ================================================= 
+;; Expression results are computed as values
 
 (env 'f)
 ;; => #<core$inc clojure.core$inc@4648acd3>    
 ;;    i.e. a Clojure function, looked up via the var #'clojure.core/inc
 
 (env 'baz)
-;; => 3
+;; => 4
 ;;    i.e. the expression (+ foo bar) has been computed
+
+
+;; =================================================
+;; Changing an environment
+;; - leaves original environment unchanged
+;; - causes update of dependent Expressions (!!!!!!)
+
+(let [env2 (assoc env 'foo 2)]
+  [(env 'foo) (env2 'foo)])
+; => [1 2]
+
+
+;; ====================================================================
+;; Kiss code can be executed against any envirnment
+;; ====================================================================
+
+(kiss env (f foo))
+;; => 2
+
+;; ================================================= 
+;; Clojure functions can also be used
+
+(kiss env (clojure.core/range bar))
+;; => (0 1 2)
